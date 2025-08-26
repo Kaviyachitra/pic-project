2b8e69294453477ade77356435f3a10638ade526
kaviya <kaviyachitra2003@gmail.com>
2025-08-26 11:29:11 +0530

    Add files via upload

---
+++ SPI program.c
@@ -0,0 +1,27 @@
+#include<pic.h>
+void delay_ms(unsigned int x)
+{
+while(x--);
+}
+void SPI_Master_Init() 
+{
+TRISC3=0;
+TRISC5=0;
+SSPSTAT=0x00;
+SSPCON=0x20;
+}
+void SPI_Write(char data)
+{
+SSPBUF = data;
+while (!SSPIF);
+SSPIF = 0;
+}
+void main() 
+{
+SPI_Master_Init();
+while(1)
+{
+SPI_Write('A');
+delay_ms(65000);
+    }
+}
